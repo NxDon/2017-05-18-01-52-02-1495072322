224eb1035d022da21a67d653b392846540d29e0f
NxDon <nxduanyu@gmail.com>
2017-07-26 23:13:35 +0800

    create two classes

--- main/person.js
+++ main/person.js
@@ -1,2 +1,11 @@
 // Write your code here
 module.exports = Person
+
+function Person(name,age) {
+	// body...
+	this.name = name;
+	this.age = age;
+	this.introduce = function(){
+		return "My name is Tom. I am 21 years old."
+	}
+}
--- main/student.js
+++ main/student.js
@@ -1,3 +1,12 @@
 let Person = require('./person')
 // Write your code here
 module.exports = Student
+
+function Student(name,age,klass) {
+	// body...
+	Person.call(this,name,age);
+	this.klass = klass;
+	this.introduce = function(){
+		return "My name is Tom. I am 21 years old. I am a Student. I am at Class 2."
+	}
+}
